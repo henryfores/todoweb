5a9b37943b61de3df764ac8b9c4d052211e19d9b
henryfores <henryfores@hotmail.com>
2019-11-04 20:03:25 -0300

    Create tododb-dml.sql

---
+++ Scripts/tododb-dml.sql
@@ -0,0 +1,20 @@
+CREATE TABLE `persona` (
+ `id` int(11) NOT NULL AUTO_INCREMENT,
+ `alias` varchar(25) NOT NULL,
+ `nombre` varchar(25) NOT NULL,
+ `apellido` varchar(25) DEFAULT NULL,
+ `email` varchar(100) NOT NULL,
+ PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1
+
+CREATE TABLE `tarea` (
+ `ID` int(11) NOT NULL AUTO_INCREMENT,
+ `DESCRIPCION` varchar(200) NOT NULL,
+ `FECHA_LIMITE` date DEFAULT NULL,
+ `ID_PERSONA` int(11) NOT NULL,
+ `REALIZADA` tinyint(1) NOT NULL,
+ `FECHA_REALIZADA` date NOT NULL,
+ PRIMARY KEY (`ID`),
+ KEY `ID_PERSONA` (`ID_PERSONA`),
+ CONSTRAINT `tarea_ibfk_1` FOREIGN KEY (`ID_PERSONA`) REFERENCES `persona` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
